01e0e39f6d1a8fc4380f0fd69c71d3263aca29fb
cris93g <37388115+cris93g@users.noreply.github.com>
2020-05-14 21:21:12 -0500

    idk

---
+++ index.js
@@ -0,0 +1,25 @@
+const navSlide = () => {
+	const burger = document.querySelector('.burger');
+	const nav = document.querySelector('.nav-links');
+	const navLinks = document.querySelectorAll('.nav-links li');
+
+	burger.addEventListener('click', () => {
+		//
+		nav.classList.toggle('nav-active');
+		//
+		navLinks.forEach((link, ind) => {
+			if (link.style.animation) {
+				link.style.animation = '';
+			} else {
+				link.style.animation = `navLinkFade 0.5s ease forwards ${ind / 7 + 1.0}s`;
+			}
+		});
+		burger.classList.toggle('toggle');
+	});
+};
+
+const app = () => {
+	navSlide();
+};
+
+app();
