ccc75afd19b7c34f89240a727a7428c42664508b
cris93g <cristhianswork@gmail.com>
2020-05-15 08:44:26 -0500

    finish up some syuff

---
+++ contact.js
@@ -0,0 +1,27 @@
+document.addEventListener('DomContentLoaded',()=>{
+    fields.name = document.getElementById('name')
+    fields.email = document.getElementById('email')
+    fields.question = document.getElementById('question')
+})
+
+function isNotEmpty(value) {
+    if (value == null || typeof value == 'undefined') return false;
+    return (value.length > 0);
+}
+
+function isEmail(email) {
+    let regex = /^/
+}
+
+function fieldValidation(field, validationFunction) {
+    if (field == null) return false;
+
+    let isFieldValid = validationFunction(field.value)
+    if (!isFieldValid) {
+        field.className = 'placeholderRed';
+    } else {
+        field.className = '';
+    }
+
+    return isFieldValid;
+}
